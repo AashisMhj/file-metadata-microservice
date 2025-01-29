78d0fa46fc1971059f229e8b411f7fc87ea0342f
Aashismhj <aashismhj5511@gmail.com>
2025-01-29 10:45:39 +0545

    implemented file upload

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
 var app = express();
+
+const upload = multer({ storage: multer.memoryStorage()});
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -10,6 +13,18 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if(!req.file){
+    return res.status(400).json({ error: 'No file uploaded'});
+  }
+
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 
 
